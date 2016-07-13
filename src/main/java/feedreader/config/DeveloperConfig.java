9502f125d819cc4fbcb81bfc9d8ac864400bf094
Steven Liechti <steven@liechti.de>
2016-07-13 08:02:43 -0400

    don't start fetching/validating threads by default for developers

--- src/main/java/feedreader/config/DeveloperConfig.java
+++ src/main/java/feedreader/config/DeveloperConfig.java
@@ -1,9 +1,8 @@
 package feedreader.config;
 
-
-public class DeveloperConfig 
-{
-    public static void run()
-    {
+public class DeveloperConfig {
+    public static void run() {
+        FeedAppConfig.FETCH_RUN_START_FETCHING = false;
+        FeedAppConfig.FETCH_RUN_START_VALIDATION = false;
     }
 }
